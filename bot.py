df9b441989769e6ccb3f65242a13657bb18941d2
Majevdiyayash <majevdiyayash@gmail.com>
2026-09-06 22:51:59 +0530

    Enable message_content intent and clean up logs

--- bot.py
+++ bot.py
@@ -43,6 +43,7 @@ class MyBot(commands.Bot):
     def __init__(self):
         intents = discord.Intents.default()
         intents.members = True
+        intents.message_content = True
         super().__init__(command_prefix="!", intents=intents)
 
     async def setup_hook(self):
